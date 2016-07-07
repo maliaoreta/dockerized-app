b1acab3211861a301e9ecc26ff95a01b3cc170f9
maliaoreta <maliaoreta@gmail.com>
2016-07-06 18:12:22 -1000

    pg-promise working

--- app-server/index.js
+++ app-server/index.js
@@ -3,11 +3,20 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const pgp = require('pg-promise')();
+const DB_CONN = process.env.DB_CONN || 'postgres://malia:password@localhost:5432/demo_app_server';
+
+var db = pgp(DB_CONN);
+
+db.many('select * from counter')
+  .then( (data) => console.log(data) )
+  .catch( (err) => console.error(err) );
+
 
 let counter = { count: 0 };
 
 app.get('/api/counter', (req, res) => res.json(counter) );
-app.get('/api/counter/increment', (req, res) => { counter.count++; res.json(counter); );
-app.get('/api/counter/decrement', (req, res) => { counter.count--; res.json(counter); );
+app.get('/api/counter/increment', (req, res) => { counter.count++; res.json(counter); } );
+app.get('/api/counter/decrement', (req, res) => { counter.count--; res.json(counter); } );
 
 app.listen ( PORT );
