ba9095ce3dbc22d9d4a43bc26b122e7438462fdf
maliaoreta <maliaoreta@gmail.com>
2016-07-08 14:38:42 -1000

    seeds db

--- app-server/index.js
+++ app-server/index.js
@@ -8,31 +8,54 @@ const DB_CONN = process.env.DB_CONN || 'postgres://malia:password@localhost:5432
 
 var db = pgp(DB_CONN);
 
+var checkTableExistsSQL = `SELECT EXISTS (
+   SELECT 1
+   FROM   information_schema.tables 
+   WHERE  table_schema = 'public'
+   AND    table_name = 'counter'
+);`
+
+var createTableSQL = `CREATE TABLE counter (
+  id serial primary key,
+  value int
+);`
+
+db.one(checkTableExistsSQL)
+  .then(data => {
+    if (!data.exists) {
+      db.query(createTableSQL)
+        .catch( err => res.json(err) )
+      db.query(`insert into counter (value) values (10), (20), (30)`)
+        .catch( err => res.json(err) )
+    }
+  })
+  .catch( err => res.json(err) )
+
 
 app.get('/api/counters', (req, res) => 
   db.many('select * from counter')
     .then( data => res.json(data) )
-    .catch( err => res.send(err) )
+    .catch( err => res.json(err) )
   );
 
 app.get('/api/counter/:id', (req, res) => 
   db.one(`select * from counter where id = ${parseInt(req.params.id)}`)
     .then( data => res.json(data) )
-    .catch( err => res.send(err) )
+    .catch( err => res.json(err) )
   );
 
 app.get('/api/counter/:id/increment', (req, res) =>
   db.one(`select * from counter where id = ${parseInt(req.params.id)}`)
     .then( data => db.query(`update counter SET value = ${data.value + 1} where id = ${data.id}`) )
     .then( data => res.json({ updated: 1 }) )
-    .catch( err => res.send(err) )
+    .catch( err => res.json(err) )
   );
 
 app.get('/api/counter/:id/decrement', (req, res) =>
   db.one(`select * from counter where id = ${parseInt(req.params.id)}`)
     .then( data => db.query(`update counter SET value = ${data.value - 1} where id = ${data.id}`) )
     .then( data => res.json({ updated: 1 }) )
-    .catch( err => res.send(err) )
+    .catch( err => res.json(err) )
   );
 
 app.listen ( PORT );
